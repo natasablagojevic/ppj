5e3adec641afb2fd7ec9e17a8c7b1e294b572dec
natasablagojevic <blagojevicnatasa360@gmail.com>
2022-11-09 18:51:35 +0100

    (a|b)*abb

---
+++ automati/automat1.py
@@ -0,0 +1,32 @@
+import sys
+
+#   (a|b)*abb
+prelazi = {("AC", "a") : "B", ("AC", "b") : "AC", ("B", "a"):"B", ("B", "b") : "D", ("D", "a"):"B", ("D", "b"):"E", ("E", "a"):"B", ("E", "b") : "AC"}
+
+pocetno = "AC"
+zavrsna = ["E"]
+
+stanje = pocetno
+
+while True:
+    try:
+        c = input("Unesi slovo\n")
+        if c != "a" and c != "b":
+            raise ValueError("nije deo alfabeta")
+    except EOFError:
+        break
+    except ValueError as err:
+        print(err)
+        sys.exit(err)
+
+    if prelazi.get((stanje, c)) is None:
+        print("Rec nije deo jezika")
+        sys.exit()
+    
+    stanje = prelazi[(stanje, c)]
+    print("\t", stanje)
+
+if stanje in zavrsna:
+    print("Rec je deo jezika")
+else 
+    print("Rec nije deo jezika")
